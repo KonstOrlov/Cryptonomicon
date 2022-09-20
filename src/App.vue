0785f86ce0b2d194a07e7c77a8e34762ad2e3bce
Konstantin Orlov <konstorlovrus@gmail.com>
2022-09-20 22:59:05 +0500

    feature: save tickers in LS

--- src/App.vue
+++ src/App.vue
@@ -141,7 +141,28 @@ export default {
     };
   },
 
+  created() {
+    const tickersData = localStorage.getItem('cryptonomicon-list')
+    if(tickersData){
+      this.tickers = JSON.parse(tickersData)
+      this.tickers.forEach(ticker => this.subscribeToUpdates(ticker.name))
+    }
+  },
+
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=543fddd238bae987073644aad39e0ac448c4c5e65dcc58535d3d62d7f720c3e5`)
+        const data = await f.json();
+
+        this.tickers.find(t => t.name === tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if(this.sel?.name === tickerName) {
+          this.graph.push(data.USD)
+        }
+      }, 5000);
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -149,16 +170,10 @@ export default {
       };
 
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=543fddd238bae987073644aad39e0ac448c4c5e65dcc58535d3d62d7f720c3e5`)
-        const data = await f.json();
 
-        this.tickers.find(t => t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
 
-        if(this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD)
-        }
-      }, 5000);
+      this.subscribeToUpdates(currentTicker.name)
       this.ticker = "";
     },
 
