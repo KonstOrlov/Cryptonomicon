746458d80464d55db86ab52d7a97d9cfa03dc762
Konstantin Orlov <konstorlovrus@gmail.com>
2022-10-06 23:07:10 +0600

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Medium-Clone/'
+    ? '/Cryptonomicon/'
     : '/',
   transpileDependencies: true
 })
