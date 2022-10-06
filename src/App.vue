83b38f3eeb93fcadaa899b085a4d679d74abf30b
Konstantin Orlov <konstorlovrus@gmail.com>
2022-10-06 22:59:03 +0600

    fix: used nextTick to display the graph

--- src/App.vue
+++ src/App.vue
@@ -299,6 +299,8 @@ export default {
   watch: {
     selectedTicker() {
       this.graph = [];
+      this.$nextTick()
+          .then(() => this.calculateMaxGraphElements())
     },
 
     tickers() {
